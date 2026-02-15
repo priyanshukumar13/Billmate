1f0dc7c8c5b1f7f44441627d7dae8c0d17379e45
Priyanshu kumar <pk2525507@gmail.com>
2026-02-16 00:29:39 +0530

    Billmate Node backend ready

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,60 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
-from db import db, cursor
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+# MongoDB Connection
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["billmate"]
+users_collection = db["users"]
+
 @app.route("/")
 def home():
-    return "BillReminder backend running successfully!"
+    return "BillMate backend running with MongoDB!"
 
+# ---------------- SIGNUP ----------------
 @app.route("/signup", methods=["POST"])
 def signup():
     data = request.json
-    print("🔥 SIGNUP HIT 🔥")
-    print("DATA RECEIVED:", data)
 
-    hashed = generate_password_hash(data["password"])
-    cursor.execute(
-        "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
-        (data["name"], data["email"], hashed)
-    )
-    db.commit()
+    # check if user exists
+    existing_user = users_collection.find_one({"email": data["email"]})
+    if existing_user:
+        return jsonify({"message": "User already exists"}), 400
 
-    print("✅USER INSERTED")
+    hashed_password = generate_password_hash(data["password"])
+
+    user_data = {
+        "name": data["name"],
+        "email": data["email"],
+        "password": hashed_password
+    }
+
+    users_collection.insert_one(user_data)
 
     return jsonify({"message": "Signup successful"}), 201
 
 
+# ---------------- LOGIN ----------------
 @app.route("/login", methods=["POST"])
 def login():
     data = request.json
-    cursor.execute("SELECT * FROM users WHERE email=%s", (data["email"],))
-    user = cursor.fetchone()
+
+    user = users_collection.find_one({"email": data["email"]})
+
     if user and check_password_hash(user["password"], data["password"]):
         return jsonify({"message": "Login successful"}), 200
+
     return jsonify({"message": "Invalid credentials"}), 401
+
 
 if __name__ == "__main__":
     app.run(debug=True)
